677bf01f6f5d9dc3ec5eed45cc3f7d3d34cf394c
Ramameiyappan <meiyappanvdm@gmail.com>
2026-02-04 14:59:40 +0530

    Auto create superuser

--- django_project/manage.py
+++ django_project/manage.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    import django
+    django.setup()
+
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+
+    admin_username = os.getenv("ADMIN_USERNAME")
+    admin_email = os.getenv("ADMIN_EMAIL")
+    admin_password = os.getenv("ADMIN_PASSWORD")
+
+    if admin_username and admin_password:
+        if not User.objects.filter(username=admin_username).exists():
+            User.objects.create_superuser(
+                username=admin_username,
+                email=admin_email,
+                password=admin_password
+            )
+            print("Superuser created")
+
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
